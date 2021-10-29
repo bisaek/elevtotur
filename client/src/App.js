31c7f638a292ebcb421d6885744f318c7f130b0c
bisaek <59936920+bisaek@users.noreply.github.com>
2021-10-29 22:22:30 +0200

    installing tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app text-white">
+      <header className="bg-gray-900">
+        hej
       </header>
+      <div className="container">
+
+      </div>
     </div>
   );
 }
