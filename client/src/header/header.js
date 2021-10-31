ddab427229276a330cadac2872cf9e9b6f1db340
bisaek <59936920+bisaek@users.noreply.github.com>
2021-10-31 12:34:09 +0100

    create header logo

--- client/src/header/header.js
+++ client/src/header/header.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import Logo from './logo'
 
 export default class Header extends Component {
 	render() {
 		return (
 			<header className="bg-gray-900 h-14">
-        hej
+        <ul className="flex">
+					<Logo></Logo>
+				</ul>
       </header>
 		)
 	}
---
+++ client/src/header/logo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class logo extends Component {
+	render() {
+		return (
+			<li className="m-1.5">
+				<h1 className="hover:bg-gray-800 p-1 text-2xl rounded-md">
+					<a href="/">elevtotur</a>
+				</h1>
+			</li>
+		)
+	}
+}
+	
