f071a269b7105859f0ed160ca4db1aadb30e2542
bisaek <59936920+bisaek@users.noreply.github.com>
2021-11-03 07:37:51 +0100

    create login button

--- client/src/header/header.js
+++ client/src/header/header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Logo from './logo'
+import Login from './login'
 
 export default class Header extends Component {
 	render() {
 		return (
-			<header className="bg-gray-900 h-14">
+			<header className="bg-gray-900 h-14 sticky">
         <ul className="flex">
 					<Logo></Logo>
+					<Login></Login>
 				</ul>
       </header>
 		)
---
+++ client/src/header/login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+
+
+	render() {
+		return (
+			<li className="absolute inset-y-0 right-6">login</li>
+		)
+	}
+}
